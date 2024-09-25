7e21b728145aa483af578dc317f1c6c0c5eb701e
Jell <mystral7@naver.com>
2024-09-25 21:38:12 +0900

    [Index] set index

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-import': {},
     tailwindcss: {},
-  },
-};
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  }
+}
 
-export default config;
+export default config
